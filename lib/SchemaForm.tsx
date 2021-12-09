984ed81cbe60fb3b905953d202982a966ca82bef
li_j604 <775837740@qq.com>
2021-12-09 13:57:45 +0800

    finished chapter4-4

---
+++ lib/SchemaForm.tsx
@@ -0,0 +1,30 @@
+import { defineComponent, PropType } from "vue";
+import { Schema, SchemaTypes } from "./types";
+
+export default defineComponent({
+  name: "SchemaForm",
+  props: {
+    schema: {
+      type: Object as PropType<Schema>,
+      required: true,
+    },
+    value: {
+      required: true,
+    },
+    onChange: {
+      type: Function as PropType<(v: any) => void>,
+      required: true,
+    },
+  },
+  setup: (props, { slots, emit, attrs }) => {
+    return () => {
+      const schema = props.schema;
+      const type = schema?.type;
+      switch (type) {
+        case SchemaTypes.STRING:
+          return <input type="text"></input>;
+      }
+      return <div>This is Form</div>;
+    };
+  },
+});
